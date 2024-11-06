5b1ae7198bae03eb3c3b7dcce3dc00120758ddc0
HOSSOUS <dossoumariestella@gmail.com>
2024-11-06 21:48:08 +0100

    my middlewear

---
+++ base de donnée/middlewear/middle.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'Accès non autorisé' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token invalide' });
+        }
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
